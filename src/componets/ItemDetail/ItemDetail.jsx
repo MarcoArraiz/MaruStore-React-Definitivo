ebd5258e9786c3c86216d8a5dc8697329d4288f2
MarcoArraiz <marcoarraiz@gmail.com>
2023-06-06 08:25:23 -0400

    up

--- src/componets/ItemDetail/ItemDetail.jsx
+++ src/componets/ItemDetail/ItemDetail.jsx
@@ -1,6 +1,15 @@
 import './ItemDetail.css'
+import ItemCount from '../ItemCount/ItemCount'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const ItemDetail = ({ id, nombre, precio, img }) => {
+const ItemDetail = ({ id, nombre, precio, img, stock }) => {
+    const [agregarCantidad, setAgregarCantidad] = useState(0);
+    const manejadorCantidad = (cantidad) => {
+        setAgregarCantidad(cantidad);
+        console.log("Productos agregados:" + cantidad);
+    }
+
     return (
         <div class=" container-fluid card mb-3" style={{ width: '600px' }}>
             <img src= {img} class="card-img-top" alt= {nombre} />
@@ -9,6 +18,9 @@ const ItemDetail = ({ id, nombre, precio, img }) => {
                     <p class="card-text"> {precio} </p>
                     <p class="card-text"> {id} </p>
                 </div>
+            {
+                agregarCantidad > 0 ? (<link to="/cart"> Terminar de comprar </link>) : (<ItemCount inicial={1} stock={stock} funcionAgregar={manejadorCantidad} />)
+            }
         </div>
     )
 }
